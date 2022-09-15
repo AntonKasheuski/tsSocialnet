1eb41fe62d756d9fccd4e5fcc9c5799e644c2706
Anton Kasheuski <kascheikinen@gmail.com>
2022-09-15 13:24:02 +0300

    add nav styles

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import s from './Navbar.module.css';
-import {NavLink} from 'react-router-dom';
+import {NavLink, useLocation} from 'react-router-dom';
 
 export function Navbar() {
+    const location = useLocation()
+
+    let navArray = [
+        {link: '/profile', name: 'Profile'},
+        {link: '/dialogs', name: 'Messages'},
+        {link: '/users', name: 'Users'},
+        {link: '/news', name: 'News'},
+        {link: '/music', name: 'Music'},
+        {link: '/settings', name: 'Settings'},
+    ]
+
     return (
         <nav className={s.nav}>
-            <NavLink to='/profile' className={navData => navData.isActive ? s.activeLink : s.item}>
-                <div className={s.navbarElement}>Profile</div>
-            </NavLink>
-            <NavLink to='/dialogs' className={navData => navData.isActive ? s.activeLink : s.item}>
-                <div className={s.navbarElement}>Messages</div>
-            </NavLink>
-            <NavLink to='/users' className={navData => navData.isActive ? s.activeLink : s.item}>
-                <div className={s.navbarElement}>Users</div>
-            </NavLink>
-            <NavLink to='/news' className={navData => navData.isActive ? s.activeLink : s.item}>
-                <div className={s.navbarElement}>News</div>
-            </NavLink>
-            <NavLink to='/music' className={navData => navData.isActive ? s.activeLink : s.item}>
-                <div className={s.navbarElement}>Music</div>
-            </NavLink>
-            <NavLink to='/settings'
-                     className={navData => navData.isActive ? s.activeLink : s.item}>
-                <div className={s.navbarElement}>Settings</div>
-            </NavLink>
+            {
+                navArray.map(el => {
+                    return <NavLink key={el.name} to={el.link}
+                                    className={location.pathname === el.link ? s.activeLink : s.item}>
+                            {location.pathname === el.link && <div className={s.activeLabel}></div>}
+                            <div className={location.pathname === el.link ? s.activeText : s.text}>{el.name}</div>
+                    </NavLink>
+                })
+            }
         </nav>
     )
 }
